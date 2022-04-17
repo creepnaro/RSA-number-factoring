5c6df7c3cfdf190f28b8affbe7769870219f0ca5
creepnaro <64645822+creepnaro@users.noreply.github.com>
2022-04-17 16:31:00 -0400

    Update factor_more.py

--- factor_more.py
+++ factor_more.py
@@ -4,6 +4,8 @@ number = int(input("Please insert a number: "))
 
 divisor = 2
 
+end_number_check = number - 1
+
 divisible = False
 remainder = True
 
@@ -12,33 +14,28 @@ divisor_check = False
 
 prime = False
 
-def isprime(num): # checks if number is prime
-    for n in range(2,int(num**0.5)+1):
-        if num%n==0:
-            return False
-    return True
+def isprime(n):
+	if n % 2 == 1:
+		return True
+	else:
+		return False
+    
 
 def check_no_remainder():
-    if new_number == int(new_number): # checks too see if new number is an intiger 
-        for i in range(number):
-            if new_number % (i+1) == 0 and divisor % (i+1) == 0:
-                remainder = True
+    if new_number/int(new_number) == 1: # checks too see if new number is an integer 
+        remainder = True
 
-while divisor != (int(number))**2 - 1 and divisible == False:
+while divisible == False:
 	new_number = number/divisor
 	check_no_remainder()
-	print("...")
-	divisor = divisor + 1
 	if remainder == True:
-	    number_check = isprime(new_number)
-	    divisor_check = isprime(divisor)
-	if number_check == True and divisor_check == True:
-	    print(number, "is divisible by", new_number, "and", divisor, sep=" ")
-	    number_check = False
-	    divisor_check = False
-	    divisble = True
-	
-if number_check == False and divisor_check == False:
-    print(number, "is not a semi-prime.", sep=" ")
-    quit()
+	    number_check = isprime(round(new_number))
+	    divisor_check = isprime(round(divisor))
+	if new_number == int(new_number) and divisor == int(divisor):
+	    print(number, "is divisible by", number/divisor, "and", divisor, sep=" ")
+	    quit()
+	if divisor == end_number_check:
+		print(number, "is not a semi-prime.", sep=" ")
+		quit()
+	divisor += 1
 quit()
